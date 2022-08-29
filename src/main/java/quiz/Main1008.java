71176c6f6f24df74da5ea1819f343decac86047a
jyung17 <iotaxixi@gmail.com>
2022-08-29 17:42:09 +0900

    0829

--- src/main/java/quiz/Main1008.java
+++ src/main/java/quiz/Main1008.java
@@ -2,13 +2,13 @@ package quiz;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main1008 {
   
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
-    String b = scanner.nextLine();
-    System.out.println(a);
-    System.out.println(b);
+    int b = scanner.nextInt();
+    
+    System.out.println((double) a / b);
   }
 }
